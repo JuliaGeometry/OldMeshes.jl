e82995dcb8692473534c88c6cc3ee661c640b4a5
Alan Bahm <alanbahm@gmail.com>
2014-04-12 08:01:29 -0700

    Perturbing vertices on null-faces, removing clean()
    Forcing the vertex coordinate to be bounded away from either end of the
    segments joining voxel corners

--- src/isosurface.jl
+++ src/isosurface.jl
@@ -162,6 +162,8 @@ function vertPos{T<:Real}(e::Int64, x::Int64, y::Int64, z::Int64,
     srcVal = float(vals[ixs[1]])
     tgtVal = float(vals[ixs[2]])
     a = (float(iso)-srcVal)/(tgtVal-srcVal)
+    const EPS = 0.001
+    a = min(max(a,EPS),1.0-EPS)
     b = 1.0-a
     org = float([x,y,z])
     src = org+float(voxCrnrPos[:,ixs[1]])
