3629642b4a2719158379907d4bb7587ee613f5bc
Alan Bahm <alanbahm@gmail.com>
2014-04-11 21:04:41 -0700

    Adding functionality to "clean"
    Adding clean() which removes zero-area faces and duplicate vertices.

--- src/core.jl
+++ src/core.jl
@@ -30,4 +30,54 @@ function merge(m1::AbstractMesh, m2::AbstractMesh)
     Mesh(append!(v1,v2),append!(f1,newF2))
 end
 
-export Vertex, Face, AbstrctMesh, Mesh, vertices, faces, merge
+function area(v1::Vertex, v2::Vertex, v3::Vertex)
+    # by Heron's formula (wikipedia)
+    a = Base.norm(v2-v1)
+    b = Base.norm(v3-v2)
+    c = Base.norm(v1-v3)
+    s = (a+b+c)/2
+    T = sqrt(s*(s-a)*(s-b)*(s-c))
+end
+
+function clean(mesh::Mesh)
+    # return a "clean" version of the mesh,
+    #   by removing all faces with zero area
+    #   and removing duplicate vertices
+    faces = Face[]
+    vertices = Vertex[]
+    n = length(mesh.faces)
+    prg = Progress(n, 1.0, "Cleaning mesh...", 40)
+    for i=1:n
+        fi = mesh.faces[i]
+        v1 = mesh.vertices[fi.v1]
+        v2 = mesh.vertices[fi.v2]
+        v3 = mesh.vertices[fi.v3]
+
+        # skip any faces with zero area
+        if area(v1,v2,v3)==0
+            continue
+        end
+
+        iv1 = findfirst(vertices, v1)
+        if iv1==0
+            push!(vertices, v1)
+            iv1 = length(vertices)
+        end
+        iv2 = findfirst(vertices, v2)
+        if iv2==0
+            push!(vertices, v2)
+            iv2 = length(vertices)
+        end
+        iv3 = findfirst(vertices, v3)
+        if iv3==0
+            push!(vertices, v3)
+            iv3 = length(vertices)
+        end
+        push!(faces, Face(iv1,iv2,iv3))
+        next!(prg)
+    end
+    cleaned_mesh = Mesh(vertices, faces)
+    return cleaned_mesh
+end
+
+export Vertex, Face, AbstrctMesh, Mesh, vertices, faces, merge, clean
