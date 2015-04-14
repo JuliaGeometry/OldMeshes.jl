c4c5c591e602d908013a476d5c6434eedde530e3
Steve Kelly <kd2cca@gmail.com>
2015-04-14 18:37:51 -0400

    tighten up mesh type on slice

--- src/slice.jl
+++ src/slice.jl
@@ -1,5 +1,5 @@
 
-function Base.slice(mesh::Mesh, heights::Array{Float64}, pair=true; eps=0.00001, autoeps=true)
+function Base.slice(mesh::Mesh{Vector3{Float64}, Face{Int}}, heights::Array{Float64}, pair=true; eps=0.00001, autoeps=true)
 
     height_ct = length(heights)
     slices = [(Vector2{Float64}, Vector2{Float64})[] for i = 1:height_ct]
